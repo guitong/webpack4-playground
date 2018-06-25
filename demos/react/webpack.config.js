053a2527eae3d1908181b0687d3248a16f566ba4
Guitong Geng <guitong.geng@gmail.com>
2018-06-25 22:06:15 +0800

    chore: update

--- demos/react/webpack.config.js
+++ demos/react/webpack.config.js
@@ -7,7 +7,12 @@ module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[hash].js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   module: {
     rules: [
